bee3fe39fbe8b981361fd1ce60469a092e565c25
Longri <www.longri.de@googlemail.com>
2017-07-11 17:48:13 +0200

    initial commit after create libgdx project with gdx-setup

---
+++ android/src/de/longri/libgdx_widgets/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package de.longri.libgdx_widgets;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import de.longri.libgdx_widgets.TestGame;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new TestGame(), config);
+	}
+}
